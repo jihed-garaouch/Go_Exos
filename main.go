2656083ca14e5bd9ec1bc12b00431e248494019e
Ziad <ziadfourati11@gmail.com>
2025-02-26 11:44:50 +0100

    Ajout des routes pour ajouter et récupérer des mots

--- main.go
+++ main.go
@@ -5,15 +5,29 @@ import (
 	"net/http"
 )
 
-func main() {
+var dictionary = make(map[string]string)
 
+func main() {
 	r := gin.Default()
 
-	r.GET("/todo", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"id":   0,
-			"task": "firstTasks",
-		})
+	r.GET("/words", func(c *gin.Context) {
+		c.JSON(http.StatusOK, dictionary)
+	})
+
+	r.POST("/add", func(c *gin.Context) {
+		var request struct {
+			Word       string `json:"word"`
+			Definition string `json:"definition"`
+		}
+
+		if err := c.BindJSON(&request); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Format JSON invalide"})
+			return
+		}
+
+		dictionary[request.Word] = request.Definition
+
+		c.JSON(http.StatusOK, gin.H{"message": "Mot ajouté avec succès"})
 	})
 
 	r.Run(":8080")
